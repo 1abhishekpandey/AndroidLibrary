29b7a62162562be376b46de05ad125a92ee5c2e5
anonarya <abhishekpandey1096@gmail.com>
2021-04-09 15:26:15 +0530

    Udated AbhishekFirstLibrary to return addition of 2 number

--- AbhishekFirstLibrary/src/main/java/com/example/abhishekfirstlibrary/Abhishek.java
+++ AbhishekFirstLibrary/src/main/java/com/example/abhishekfirstlibrary/Abhishek.java
@@ -1,7 +1,21 @@
 package com.example.abhishekfirstlibrary;
 
+import android.content.Context;
+import android.widget.Button;
+
+import java.util.Date;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import android.widget.Toast;
+
 public class Abhishek {
 
+    public static int add2number(int a,int b){
+
+        return a+b;
+    }
 
 
 }
